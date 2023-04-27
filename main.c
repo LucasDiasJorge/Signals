c2b159d6919f0c981fbf1c0d8dfe67003d6c4f9c
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-04-26 23:17:06 -0300

    Updating C version

--- main.c
+++ main.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
-void handler(int num) {
-    write(STDOUT_FILENO, "I won't die!\n", 13);
+void signal_handler(int signal) {
+    printf("Sinal %d recebido\n", signal);
+    if(signal == 15){
+        exit(0);
+    }
 }
 
 int main() {
 
-    signal(SIGINT, handler);
-    signal(SIGTERM, handler);
-    signal(SIGKILL, handler);
-
-    while(1) {
-        sleep(1);
-        printf("PID = %d\n",getpid());
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // processo filho
+        printf("Filho iniciado com PID %d\n", getpid());
+        signal(SIGTERM, signal_handler);
+        while (1) {
+            printf("Processo filho rodando...\n");
+            sleep(1);
+        }
+    } else {
+        // processo pai
+        printf("Pai iniciado com PID %d\n", getpid());
+        sleep(5);
+        printf("Enviando sinal SIGSTOP para o processo filho...\n");
+        kill(pid, SIGSTOP);
+        sleep(5);
+        printf("Enviando sinal SIGCONT para o processo filho...\n");
+        kill(pid, SIGCONT);
+        sleep(5);
+        printf("Enviando sinal SIGTERM para o processo filho...\n");
+        kill(pid, SIGTERM);
+        wait(NULL);
+        printf("Processo filho finalizado\n");
     }
-
     return 0;
 }
